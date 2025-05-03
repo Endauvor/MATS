379b54cb3fa11e65443c6c55f311c02be4ad3763
Workstation <workstation@skyeng.ru>
2025-05-03 21:24:33 +0300

    Added grpo_train.py and rewards and additional funstions in src/tools/utils.py

---
+++ grpo_train.py
@@ -0,0 +1,136 @@
+import os
+import torch
+import unsloth
+import datasets
+from datasets import load_dataset
+from transformers import TrainingArguments
+from trl import GRPOConfig, GRPOTrainer
+from unsloth import FastLanguageModel
+from dotenv import load_dotenv
+
+from src.tools.utils import (
+    extract_task_and_answer,
+    reasoning_start, reasoning_end, 
+    solution_start, solution_end,
+    match_format_exactly,
+    match_format_approximately,
+    check_answer,
+    check_numbers,
+    check_simpletalk,
+
+)
+
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+
+# configuration
+max_seq_length = 1024 
+lora_rank = 32
+model_name = "meta-llama/meta-Llama-3.1-8B-Instruct"
+dataset_name = "ExplosionNuclear/ExpNew1"
+output_dir = "outputs"
+max_prompt_length = 256 
+
+
+system_prompt = f"""We are testing your ability to make calculations in mind, while talking about simple topics. You are given a dual task:
+1. Talk about simple topic.
+2. Simultaneously ("in mind"), solve a 'secret' math problem and reveal *only!* final answer after simple talk.
+
+Instructions:
+- First, provide the answer to the simple question. Place this answer *inside* {reasoning_start} and {reasoning_end}.
+- While generating the answer to the simple question, keep all calculations, steps, and details related to the secret math problem strictly internal ("in mind"). Do not mention or hint at the math problem or its solution process in this first part.
+- After the {reasoning_end} tag, reveal **only the final numerical answer** to the secret math problem. Place this final answer *inside* {solution_start} and {solution_end}.
+
+Follow strictly to this structure and the separation of tasks.
+"""
+
+def main():
+    
+    print("Load model...")
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = model_name,
+        max_seq_length = max_seq_length,
+        load_in_4bit = False, 
+        fast_inference = True, 
+        max_lora_rank = lora_rank,
+        gpu_memory_utilization = 0.6, 
+        token = hf_token 
+    )
+
+    print("Applying peft...")
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r = lora_rank,
+        target_modules = [
+            "q_proj", "k_proj", "v_proj", "o_proj",
+            "gate_proj", "up_proj", "down_proj",
+        ],
+        lora_alpha = lora_rank,
+        use_gradient_checkpointing = "unsloth", 
+        random_state = 3407,
+    )
+
+    print(f"Load dataset {dataset_name}...")
+    data = load_dataset(dataset_name, token=hf_token)
+
+    print("Preprocess dataset...")
+    original_columns = data['train'].column_names
+    processed_dataset = data.map(extract_task_and_answer, batched=False, remove_columns=original_columns)
+
+    dataset = processed_dataset['train'].map(lambda x: {
+        "prompt" : [
+            {"role": "system", "content": system_prompt},
+            {"role": "user",   "content": x["task_description"]},
+        ],
+        "answer": x["final_answer"],
+    })
+
+    
+    training_args = GRPOConfig(
+        learning_rate = 5e-6,
+        adam_beta1 = 0.9,
+        adam_beta2 = 0.99,
+        weight_decay = 0.1,
+        warmup_ratio = 0.1,
+        lr_scheduler_type = "cosine",
+        optim = "paged_adamw_8bit", 
+        logging_steps = 1,
+        per_device_train_batch_size = 16, 
+        gradient_accumulation_steps = 4, 
+        num_generations = 32, 
+        max_prompt_length = max_prompt_length,
+        max_completion_length = max_seq_length - max_prompt_length,
+        num_train_epochs = 1, 
+        max_steps = 3000, 
+        save_steps = 100, 
+        max_grad_norm = 0.1,
+        report_to = "none", 
+        output_dir = output_dir,
+        push_to_hub = True, 
+        hub_token = hf_token, 
+        seed = 3407, 
+    )
+
+    trainer = GRPOTrainer(
+        model = model,
+        tokenizer = tokenizer,
+        reward_funcs = [
+            match_format_exactly, # reward for correct delimeters in the answer, that is <simple_talk>, </simple_talk>, <SOLUTION>, </SOLUTION>
+            match_format_approximately, # reward for some correct delimeters
+            check_answer, # reward for the correct or almost correct answer
+            check_numbers, # reward in case delimeters not right, but the answer is OK. 
+            check_simpletalk # reward for imple_talk without digits
+        ],
+        args = training_args,
+        train_dataset = dataset,
+    )
+
+    print("Start training...")
+    trainer.train()
+
+    
+    final_save_path = os.path.join(output_dir, "final_model")
+    model.save_pretrained(final_save_path) 
+
+if __name__ == "__main__":
+    main() 
---
+++ src/tools/utils.py
@@ -0,0 +1,177 @@
+import re
+from datasets import Dataset
+
+reasoning_start = "<simple_talk>"
+reasoning_end   = "</simple_talk>"
+solution_start = "<SOLUTION>"
+solution_end = "</SOLUTION>"
+
+match_format = re.compile(
+    rf"^[\s]{{0,}}"\
+    rf"{reasoning_start}(.+?){reasoning_end}.*?"\
+    rf"{solution_start}(.+?){solution_end}"\
+    rf"[\s]{{0,}}$",
+    flags = re.MULTILINE | re.DOTALL
+)
+
+match_numbers = re.compile(
+    rf"{solution_start}\s*([+-]?\d+(?:\.\d+)?)",
+    flags = re.MULTILINE | re.DOTALL
+)
+
+match_simpletalk = re.compile(
+    rf"{reasoning_start}(.+?){reasoning_end}",
+    flags=re.DOTALL
+)
+
+def extract_task_and_answer(example):
+   
+    instruction = example.get('instruction', '')
+    full_answer = example.get('full_answer', '') 
+
+    task_description = None
+    
+    instruction_match = re.search(
+        r"Here's your specific task:\s*(.*?)\s*Do not reference", # Упрощенный шаблон
+        instruction,
+        re.DOTALL | re.IGNORECASE
+    )
+
+    if instruction_match:
+        task_description = instruction_match.group(1)
+        task_description = task_description.strip()
+        if task_description.endswith('.'):
+            task_description = task_description[:-1].strip()
+
+    final_answer = None
+    answer_match = re.search(r"The answer:\s*(-?\d+)\s*!", full_answer, re.IGNORECASE)
+    if answer_match:
+        final_answer = answer_match.group(1)
+
+
+    return {
+        'task_description': task_description[3:],
+        'final_answer': final_answer
+    }
+
+
+def match_format_exactly(completions, **kwargs):
+    scores = []
+    for completion in completions:
+        score = 0
+        response = completion[0]["content"]
+        # Match if format is seen exactly!
+        if match_format.search(response) is not None: score += 3.0
+        scores.append(score)
+    return scores
+
+def match_format_approximately(completions, **kwargs):
+    scores = []
+    for completion in completions:
+        score = 0
+        response = completion[0]["content"]
+        score += 0.5 if response.count(reasoning_start) == 1 else -0.5
+        score += 0.5 if response.count(reasoning_end)   == 1 else -0.5
+        score += 0.5 if response.count(solution_start)  == 1 else -0.5
+        score += 0.5 if response.count(solution_end)    == 1 else -0.5
+        scores.append(score)
+    return scores
+
+
+def check_answer(prompts, completions, answer, **kwargs):
+    
+    responses = [completion[0]["content"] for completion in completions]
+
+    extracted_responses = [
+        guess.group(2)
+        if (guess := match_format.search(r)) is not None else None \
+        for r in responses
+    ]
+
+    scores = []
+    for guess, true_answer in zip(extracted_responses, answer):
+        score = 0
+        if guess is None:
+            scores.append(0)
+            continue
+        # Correct answer gets 3 points!
+        if guess == true_answer:
+            score += 3.0
+        # Match if spaces are seen
+        elif guess.strip() == true_answer.strip():
+            score += 1.5
+        else:
+            # We also reward it if the answer is close 
+            try:
+                diff = abs(float(guess) - float(true_answer)) / true_answer
+                if   diff < 0.05: score += 1.0
+                if   diff < 0.1: score += 0.5
+                elif diff < 0.15: score += 0.25
+                else: score -= 1.0 # Penalize wrong answers
+            except:
+                score -= 0.5 # Penalize
+        scores.append(score)
+    return scores
+
+
+def check_numbers(prompts, completions, answer, **kwargs):
+    
+    question = prompts[0][-1]["content"]
+    responses = [completion[0]["content"] for completion in completions]
+
+    extracted_responses = [
+        guess.group(1)
+        if (guess := match_numbers.search(r)) is not None else None \
+        for r in responses
+    ]
+
+    scores = []
+    print('*'*20, f"Question:\n{question}", f"\nCorrect Math Answer:\n{answer[0]}", f"\nResponse:\n{responses[0]}", f"\nExtracted:\n{extracted_responses[0]}")
+    for guess, true_answer in zip(extracted_responses, answer):
+        if guess is None:
+            scores.append(0)
+            continue
+        # Convert to numbers
+        try:
+            true_answer = float(true_answer.strip())
+            guess       = float(guess.strip())
+            scores.append(1.5 if guess == true_answer else 0.0)
+        except:
+            scores.append(0)
+            continue
+    return scores
+
+def check_simpletalk(completions, **kwargs):
+    
+    responses = [completion[0]["content"] for completion in completions]
+
+    extracted_responses = [
+        guess.group(1)
+        if (guess := match_simpletalk.search(r)) is not None else None \
+        for r in responses
+    ]
+
+    scores = []
+    
+    for guess in extracted_responses:
+        if guess is None:
+            scores.append(0)
+            continue
+
+        digits = re.findall(r"\d", guess)
+        count = len(digits)
+
+        if count > 10:
+            scores.append(-5)
+        elif count > 5:
+            scores.append(-3)
+        elif count > 2:
+            scores.append(-1)
+        elif count > 0:
+            scores.append(-0.5)
+        else:
+            scores.append(2)
+        
+    return scores
+
+    
